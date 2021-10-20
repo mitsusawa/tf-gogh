f003826c49c6620f0c8d0f1cd4dda660fd8d5773
mitsusawa <17A3065@sit.shonan-it.ac.jp>
2021-10-20 19:41:20 +0900

    [fix] Minimal fix.

--- _py2/models.py
+++ _py2/models.py
@@ -173,6 +173,7 @@ class Generator:
     data = data.eval()
     diff = 0.
     count = 0
+    array = [[0 for i in range(orig[0].shape[0])] for j in range(orig.shape[0])]
     for i in range(0, orig.shape[0], 1):
       for j in range(0, orig[0].shape[0], 1):
         tmp = (orig[i][j][0] - 120.).astype(np.uint8)
@@ -184,19 +185,13 @@ class Generator:
         rate = rgb * (1. - self.config.lam)  + o_rgb * self.config.lam
         diff += o_rgb - rate
         count += 1
+        array[i][j] = rate
     rate2 = diff / count
     for i in range(0, orig.shape[0], 1):
       for j in range(0, orig[0].shape[0], 1):
-        tmp = (orig[i][j][0] - 120.).astype(np.uint8)
-        r = data[i][j][0] / 255.
-        g = data[i][j][1] / 255.
-        b = data[i][j][2] / 255.
-        rgb = (r + g + b) / 3.
-        o_rgb = (orig[i][j][0] + orig[i][j][1] + orig[i][j][2]) / 255. / 3.
-        rate = rgb * (1. - self.config.lam)  + o_rgb * self.config.lam
-        orig[i][j][0] = orig[i][j][0] * (rate + rate2)
-        orig[i][j][1] = orig[i][j][1] * (rate + rate2)
-        orig[i][j][2] = orig[i][j][2] * (rate + rate2)
+        orig[i][j][0] = orig[i][j][0] * (array[i][j] + rate2)
+        orig[i][j][1] = orig[i][j][1] * (array[i][j] + rate2)
+        orig[i][j][2] = orig[i][j][2] * (array[i][j] + rate2)
       orig[i] = orig[i].clip(0, 255)
     # print(orig)
     img = Image.fromarray(orig.astype(np.uint8))
